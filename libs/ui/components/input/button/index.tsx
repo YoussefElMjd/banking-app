59620354c4744d4d0396c618be7a45cd56b7cc1f
Youssef El Majdoul <Youssef.ElMajdoul@gmail.com>
2023-08-13 00:58:15 +0200

    change author email and username

--- libs/ui/components/input/button/index.tsx
+++ libs/ui/components/input/button/index.tsx
@@ -1,6 +1,7 @@
 import { Button as ChakraButton  } from '@chakra-ui/react';
 import React from 'react'
 import { IButton } from './props';
+
 export default function Button({children,...rest} : IButton){
     return <ChakraButton {...rest}>{children}</ChakraButton>
 }
